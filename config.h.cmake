f15337c62f3d1bd422eba3e22ae4e869dd41d15f
iven <ivenvd@gmail.com>
2012-02-12 23:06:35 +0800

    Hello, Qt!

---
+++ config.h.cmake
@@ -0,0 +1,11 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define APP_NAME "mingmu"
+#define APP_VERSION "0.0.1"
+#define APP_URL ""
+
+#define MM_DATA_DIR "${MM_DATA_DIR}"
+#define MM_DOC_DIR "${MM_DOC_DIR}"
+
+#endif
---
+++ src/config.h
@@ -0,0 +1,11 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define APP_NAME "mingmu"
+#define APP_VERSION "0.0.1"
+#define APP_URL ""
+
+#define MM_DATA_DIR "/usr/local/share/mingmu"
+#define MM_DOC_DIR "/usr/local/share/doc/mingmu"
+
+#endif
